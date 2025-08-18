8c3503a90f1517ca5953cd19fac008fa59344b10
luyong <2630914115@qq.com>
2025-08-18 15:56:12 +0800

    .

--- algorithm/graph/tarjan/directed_graph/SCC.java
+++ algorithm/graph/tarjan/directed_graph/SCC.java
@@ -1,75 +1,115 @@
-package algorithm.graph.tarjan.directed_graph;
-import static algorithm.zz.U.*;
+class TwoSat {
+    // range [1, n]
+    // if i is true, then i + n must be false
+    // if i is false, then i + n must be true
 
-import java.util.*;
+    private int[] stack;
+    private int[] head, nxt, to;
+    private int[] dfn, low, stk, scc;
+    private boolean[] vis;
+    private int n, z, ts, top, no;
 
-/**
- * 求有向图的强连通分量
- * 测试链接：https://www.luogu.com.cn/problem/P2863
- */
-public class SCC {
+    public TwoSat(int n) {
+        this.n = n;
+        z = 1;
+        int m = n << 1 | 1;
+        stack = new int[m];
+        head = new int[m];
+        nxt = new int[m];
+        to = new int[m];
+        dfn = new int[m];
+        low = new int[m];
+        stk = new int[m];
+        scc = new int[m];
+        vis = new boolean[m];
+    }
 
-    int[] head, nxt, to;
-    int[] dfn, low, stk, scc, sz;
-    boolean[] vis;
-    int ts, top, no;
-
-    void solve() {
-        int n = ni(), m = ni();
-        head = new int[n + 1];
-        nxt = new int[m + 1];
-        to = new int[m + 1];
-        for (int i = 0, j = 1; i < m; i++) {
-            int u = ni(), v = ni();
-            nxt[j] = head[u];
-            head[u] = j;
-            to[j++] = v;
+    public void addEdge(int u, int v) {
+        if (z == nxt.length) {
+            nxt = java.util.Arrays.copyOf(nxt, z << 1);
+            to = java.util.Arrays.copyOf(to, z << 1);
         }
-        dfn = new int[n + 1];
-        low = new int[n + 1];
-        stk = new int[n + 1];
-        scc = new int[n + 1];
-        sz = new int[n + 1];
-        vis = new boolean[n + 1];
-        ts = 0;
-        top = 0;
-        no = 0;
-        for (int u = 1; u <= n; u++) {
+        nxt[z] = head[u];
+        head[u] = z;
+        to[z++] = v;
+    }
+
+    public boolean[] match() {
+        for (int u = 1; u <= n << 1; u++) {
             if (dfn[u] == 0) {
                 tarjan(u);
             }
         }
-        int cnt = 0;
-        for (int u = 1; u <= n; u++) {
-            if (dfn[u] == low[u] && sz[scc[u]] > 1) {
-                cnt++;
+        for (int i = 1; i <= n; i++) {
+            if (scc[i] == scc[i + n]) {
+                return null;
             }
         }
-        println(cnt);
+        boolean[] ans = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            ans[i] = scc[i] < scc[i + n];
+        }
+        return ans;
     }
 
-    void tarjan(int u) {
-        dfn[u] = low[u] = ++ts;
-        stk[++top] = u;
-        vis[u] = true;
-        for (int e = head[u], v = to[e]; e != 0; e = nxt[e], v = to[e]) {
-            if (dfn[v] == 0) {
-                tarjan(v);
-                low[u] = Math.min(low[u], low[v]);
-            } else if (vis[v]) {
-                low[u] = Math.min(low[u], dfn[v]);
+    private void tarjan(int u) {
+        int z = 1;
+        stack[1] = u;
+        while (z > 0) {
+            u = stack[z];
+            if (dfn[u] == 0) {
+                dfn[u] = low[u] = ++ts;
+                stk[++top] = u;
+                vis[u] = true;
+                for (int e = head[u], v; e != 0; e = head[u]) {
+                    v = to[e];
+                    if (dfn[v] == 0) {
+                        stack[++z] = v;
+                        break;
+                    }
+                    if (vis[v]) {
+                        low[u] = Math.min(low[u], dfn[v]);
+                    }
+                    head[u] = nxt[e];
+                }
+            } else {
+                int e = head[u];
+                if (e != 0) {
+                    low[u] = Math.min(low[u], low[to[e]]);
+                    head[u] = nxt[e];
+                    e = head[u];
+                    for (int v; e != 0; e = head[u]) {
+                        v = to[e];
+                        if (dfn[v] == 0) {
+                            stack[++z] = v;
+                            break;
+                        }
+                        if (vis[v]) {
+                            low[u] = Math.min(low[u], dfn[v]);
+                        }
+                        head[u] = nxt[e];
+                    }
+                }
+                if (e == 0) {
+                    z--;
+                    if (dfn[u] == low[u]) {
+                        no++;
+                        int o;
+                        do {
+                            o = stk[top--];
+                            vis[o] = false;
+                            scc[o] = no;
+                        } while (o != u);
+                    }
+                }
             }
         }
-        if (dfn[u] == low[u]) {
-            no++;
-            int o;
-            do {
-                o = stk[top--];
-                vis[o] = false;
-                scc[o] = no;
-                sz[no]++;
-            } while (o != u);
-        }
     }
 
+    public void clear() {
+        Arrays.fill(head, 0);
+        Arrays.fill(dfn, 0);
+        z = 1;
+        ts = no = 0;
+    }
 }
